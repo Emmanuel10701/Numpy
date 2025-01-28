43fbc6fed1b5d455255243c401ecb1dff5f7e789
Emmanuel <156654447+Emmanuel10701@users.noreply.github.com>
2025-01-28 13:52:54 +0300

    Create inverse.py

---
+++ inverse.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Example matrix
+matrix = np.array([[1, 2], [3, 4]])
+
+# Finding the inverse of the matrix
+inverse_matrix = np.linalg.inv(matrix)
+
+# Display the inverse matrix
+inverse_matrix
